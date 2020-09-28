b15ad5903a99950f09233608f3eee9c8eef66021
ashfaq pathan <https://github.com/ashfaq05>
2020-09-28 23:24:53 +0530

    host demo

--- bin/dummy.py
+++ bin/dummy.py
@@ -11,8 +11,13 @@ from flask import Flask,request
 app= Flask(__name__)
 
 @app.route('/home',methods =['GET'])
-
 def checkstause():
     return "demo project"
+
+@app.route('/suma',methods =['GET'])
+def sumnum():
+    a=4
+    b=9
+    return "sum of num {} an {} is {}".format(a,b,a+b)
 
 app.run(host='localhost',port=8023) 
